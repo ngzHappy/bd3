a177be81c59b2f32cb96b65d750f99e311b00da3
ngzHappy <819869472@qq.com>
2016-11-08 17:40:45 +0800

    +update speed

--- cplusplus_basic/memory/private/memory_construct.cpp
+++ cplusplus_basic/memory/private/memory_construct.cpp
@@ -8,6 +8,7 @@ namespace __memory {
 namespace memory {
 
 std::atomic_bool _d_is_construct_static;
+char _d_once_flag[sizeof(std::once_flag)];
 
 }/*memory*/
 }/*__memory*/
@@ -28,7 +29,8 @@ void __memory__construct_static::__set_construct_static() noexcept(true) {
 bool __memory__construct_static::__run_once(void(*arg)(void)) noexcept(true) {
     if (arg==nullptr) { return false; }
     /*it will not be deleted*/
-    static auto * _d_run_once_flag=new std::once_flag;
+    static auto * _d_run_once_flag=
+        new(__cpp::__private::__memory::memory::_d_once_flag) std::once_flag;
     try {
 
         const auto call_funcs=[](void(*fun)(void)) {
