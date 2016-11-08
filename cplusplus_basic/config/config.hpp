a873bcab6c8e86fc35616a32cb3d4ffe231edd3d
ngzHappy <819869472@qq.com>
2016-11-08 16:12:48 +0800

    +construct

---
+++ cplusplus_basic/config/config.hpp
@@ -0,0 +1,25 @@
+﻿#ifndef __CONFIG_HPP_0x23786
+#define __CONFIG_HPP_0x23786
+
+/****************************************************************/
+#include <Qt/boost/config.hpp>
+#include <cstddef>
+#include <cstdbool>
+#include <cinttypes>
+#include <type_traits>
+#include <initializer_list>
+/****************************************************************/
+
+/****************************************************************/
+#if defined(CPLUSPLUS_BASIC_LIBRARYSHARED_EXPORT)
+#  define CPLUSPLUS_BASIC_LIBRARYSHARED_EXPORT BOOST_SYMBOL_EXPORT
+#else
+#  define CPLUSPLUS_BASIC_LIBRARYSHARED_EXPORT BOOST_SYMBOL_IMPORT
+#endif
+/****************************************************************/
+
+#endif
+
+
+
+
--- cplusplus_basic/cplusplus_basic.pro
+++ cplusplus_basic/cplusplus_basic.pro
@@ -5,6 +5,9 @@ include($$PWD/cplusplus_basic.pri)
 TARGET = cplusplus_basic_library
 TEMPLATE = lib
 
+include($$PWD/_private_cplusplus_basic_headers.pri)
+DEFINES*=CPLUSPLUS_BASIC_LIBRARYSHARED_EXPORT
+
 include($$PWD/../this_project.pri)
 DESTDIR=$$THIS_PROJECT_DESTDIR
 
