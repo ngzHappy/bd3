0fe97632bd8221faf34ce421e759f698cb9e7ae7
ngzHappy <819869472@qq.com>
2016-11-09 12:23:40 +0800

    +lua test

--- test/quick_test/quick_test/TestLua.cpp
+++ test/quick_test/quick_test/TestLua.cpp
@@ -1,20 +1,31 @@
 ﻿#include "TestLua.hpp"
 #include <lua/lua.hpp>
 
-TestLua::TestLua(){
+TestLua::TestLua() {
 
     auto L=luaL::newstate();
-    try {
-        lua::pushcfunction(L,[](lua::State * L)->int {
-            lua::pushlstring(L,"lua error test");
-            lua::error(L);
-            return 0;
-        });
-        lua::pcall(L,0,0,0);
-    }
-    catch (...) {
-        exception::exception_handle(false);
-    }
+
+    class ThisCall {
+    public:
+        TestLua * testLua;
+        int a;
+        double b; 
+        float c;
+    };
+    ThisCall test;
+
+    lua::pushcfunction(L,[](lua::State * L)->int {
+        auto var=reinterpret_cast<ThisCall*>(lua::touserdata(L,1));
+        var->a=1;
+        var->b=2;
+        var->c=3;
+        lua::pushlstring(L,"lua error test");
+        lua::error(L);
+        return 0;
+    });
+    lua::pushlightuserdata(L,&test);
+    lua::pcall(L,1,0,0);
+
     lua::close(L);
 
 }
