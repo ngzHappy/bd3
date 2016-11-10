39dd95aa6a7dbfed350c56c6508702afb32706c6
ngzHappy <819869472@qq.com>
2016-11-10 15:40:33 +0800

    +memory

--- tool/memory_maker/main.cpp
+++ tool/memory_maker/main.cpp
@@ -157,6 +157,9 @@ inline void make(
             }
             return nullptr;
              }/*void * malloc()*/
+        Item_N() {
+            if /*c++17 constexpr*/(N<(1024)) { _pm_pool.malloc()/*init without delete*/; }
+        }/*Item_N*/
          }/*class Item_N*/;
 
          /*+++*/
