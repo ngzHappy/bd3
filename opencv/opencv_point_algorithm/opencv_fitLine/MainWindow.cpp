f06fb92c9743676092729e4b7e686ea246a46819
ngzHappy <819869472@qq.com>
2016-11-20 17:59:27 +0800

    remove

--- opencv/opencv_point_algorithm/opencv_fitLine/MainWindow.cpp
+++ opencv/opencv_point_algorithm/opencv_fitLine/MainWindow.cpp
@@ -29,23 +29,27 @@ namespace {
 class SubImageShowWidget :public ImageShowWidget {
     class EvalLinePoint {
         using f1type_=qreal;
-        f1type_ cx0=0;
-        f1type_ cy0=0;
-        f1type_ cdx=0;
-        f1type_ cdy=0;
+        f1type_ cx0=0; f1type_ cy0=0;
+        f1type_ cdx=0; f1type_ cdy=0;
+        f1type_ cymin=0; f1type_ cymax=0;
     public:
         template<typename T0,typename T1,
-            typename T2,typename T3>
-            EvalLinePoint(T0&&a,T1&&b,T2&&c,T3&&d):
-            cx0(a),cy0(b),cdx(c),cdy(d) {
+            typename T2,typename T3,
+            typename T4,typename T5
+        >
+            EvalLinePoint(T0&&a,T1&&b,T2&&c,T3&&d,T4&&e,T5&&f):
+            cx0(a),cy0(b),cdx(c),cdy(d),cymin(e),cymax(f) {
             if (cdx==0) { return; }
             cdy/=cdx; cdx=1;
         }
 
         template<typename T0>
         f1type_ operator()(T0 && x)const {
-            if (cdx==0) { return cy0; }
-            f1type_ _dx=x-cx0;
+            const f1type_ _dx=x-cx0;
+            if (cdx==0) {
+                if (_dx>0) { return cymax; }
+                return cymin;
+            }
             return cy0+cdy*_dx;
         }
 
@@ -94,16 +98,17 @@ public:
     }
 
     void updateLine(
-        int distType,
-        double param,
-        double reps,
-        double aeps) {
+        int distType/*算法类型*/,
+        double param/*算法中的常数,如果输入0则opencv自动选择*/,
+        double reps/*距离精度*/,
+        double aeps/*角度精度*/) {
         /*opencv 曲线拟合*/
         try {
 
             auto rows=static_cast<int>(data_points2d.size());
             constexpr auto cols=2;
 
+            /*创建ref矩阵,不拷贝数据*/
             cv::Mat data(
                 rows,
                 cols/*2*/,
@@ -113,38 +118,46 @@ public:
 
             /*dx,dy,x0,y0*/
             cv::Vec4f varAns;
+
+            /*opencv直线拟合*/
             cv::fitLine(data,varAns,
                 distType,
                 param,
                 reps,
                 aeps);
 
-            {
-                auto & chart=data_chart;
-                auto xMax=chart->imageXAxis()->max();
-                auto xMin=chart->imageXAxis()->min();
-                const EvalLinePoint eval(varAns[2],varAns[3],
-                    varAns[0],varAns[1]);
-                const QList<QPointF> points{
-                    QPointF(xMin,eval(xMin)),
-                    QPointF(xMax,eval(xMax)),
-                };
-
-                auto & line=data_line;
-                if (line) {
-                    line->replace(points);
-                }
-                else {
-                    line=addLineSeries(chart,points);
-                    line->setPen(QPen(QColor(2,215,4),1.6));
-                }
-
-            }
+            /*绘制直线*/
+            this->drawLine(varAns);
 
         }
         catch (...) {
             CPLUSPLUS_EXCEPTION(false);
         }
+    }
+
+    void drawLine(const cv::Vec4f & varAns) {
+
+        auto & chart=data_chart;
+        auto xMax=chart->imageXAxis()->max();
+        auto xMin=chart->imageXAxis()->min();
+        const EvalLinePoint eval(varAns[2],varAns[3],
+            varAns[0],varAns[1],
+            chart->imageYAxis()->min(),
+            chart->imageYAxis()->max());
+        const QList<QPointF> points{
+            QPointF(xMin,eval(xMin)),
+            QPointF(xMax,eval(xMax)),
+        };
+
+        auto & line=data_line;
+        if (line) {
+            line->replace(points);
+        }
+        else {
+            line=addLineSeries(chart,points);
+            line->setPen(QPen(QColor(2,215,4),1.6));
+        }
+
     }
 
 private:
