8073c544bec4f29a3f50a24d24ada259441eaf8e
ngzHappy <819869472@qq.com>
2016-11-21 23:26:47 +0800

    +paint call back

--- opencv_basic/AbstractImageShift.hpp
+++ opencv_basic/AbstractImageShift.hpp
@@ -1,6 +1,7 @@
 ﻿#ifndef ABSTRACTIMAGESHIFT_HPP
 #define ABSTRACTIMAGESHIFT_HPP
 
+class QPainter;
 #include <QtGui/qimage.h>
 #include "OpenCVBasic.hpp"
 
@@ -10,8 +11,13 @@ public:
     virtual ~AbstractImageShift()=default;
 public:
     virtual QImage run(const QImage&) const=0;
+    virtual void paint(QPainter *,const QSize &/*image size*/){
+        /*gui系统先调用run,再调用paint*/}
 private:
     CPLUSPLUS_OBJECT(AbstractImageShift)
 };
 
 #endif // ABSTRACTIMAGESHIFT_HPP
+
+
+
