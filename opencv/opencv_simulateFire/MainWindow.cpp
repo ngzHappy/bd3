b61a421604242193edacd9cefc451330ad6bab08
ngzHappy <819869472@qq.com>
2016-11-23 17:50:18 +0800

    +simulate fire

--- opencv/opencv_simulateFire/MainWindow.cpp
+++ opencv/opencv_simulateFire/MainWindow.cpp
@@ -23,80 +23,48 @@ QWidget* MainWindow::addImage(const QImage &arg) {
 }
 
 namespace {
-
-void add_image(MainWindow *view)try {
-
-    constexpr auto rows=1024;
-    constexpr auto cols=1024;
-
-    cv::Mat mat=cv::Mat::zeros(rows,cols,CV_64FC1);
-
-    {
-        auto p=reinterpret_cast<double*>(mat.ptr(0));
-        for (auto i=0; i<cols; ++i,++p) {
-            *p+=(std::rand()&1023)/1024.0;
-        }
-    }
-
-    for(int k=0;k<200;++k)
-    {
-        auto cp=mat.ptr(0);
-        for (int i=1; i<(rows-1); ++i) {
-            auto lp=reinterpret_cast<double*>(cp)+1;
-            cp+=mat.step;
-            auto p=reinterpret_cast<double*>(cp)+1;
-            for (int j=1; j<(cols-1);++j,++p,++lp) {
-                *p+=(*(lp-1)+(*lp)+*(lp+1));
-                *p/=4;
-            }
-        }
-    }
-
-    cv::normalize(
-        mat,mat,
-        255.0,0.0,
-        cv::NORM_MINMAX,
-        CV_8UC1);
-
-    QOpencvImage image;
-    image.fromOpencvMat(mat);
-
-    view->addImage(image);
-}
-catch (...) {
-    CPLUSPLUS_EXCEPTION(false);
-}
-
-}/*namespace*/
-
-
 class FireImageShowWidget :public ImageShowWidget {
     cv::Mat fireData;
     QImage fireImage;
     int fireTimerID=0;
-    constexpr static auto rows=1024;
-    constexpr static auto cols=1024;
+    double control=0;
+    constexpr static auto rows=300;
+    constexpr static auto cols=512;
+    static_assert(rows>256,"");
 public:
     FireImageShowWidget() {
+
         fireData=cv::Mat::zeros(rows,cols,CV_64FC1);
-        fireImage=QImage(cols,rows,QImage::Format_Grayscale8);
-        /*init first line*/
-        auto p=reinterpret_cast<double*>(fireData.data);
-        auto pend=p+cols;
-        for (; p<pend;++p) {
-            *p+=(std::rand()&1023)/1024.0;
+        fireImage=QImage(cols,rows,QImage::Format_RGBA8888);
+
+        fireTimerID=this->startTimer(1000/20);
+        setOriginalImageWidgetVisible(false);
+
+        {
+            /*init first line*/
+            auto p=reinterpret_cast<double*>(fireData.data);
+            auto pend=p+cols;
+            for (; p<pend; ++p) {
+                *p=std::max(.512,std::min(.9915,(std::rand()&1023)/(512.)));
+            }
+
         }
 
-        fireTimerID=this->startTimer(1000);
-        setOriginalImageWidgetVisible(false);
+        updateFireView();
+
     }
 
     void timerEvent(QTimerEvent *e)override {
-        if (e->timerId()==fireTimerID) { updateFireView(); }
+        if (e->timerId()==fireTimerID) {
+            control+=0.05;
+            control=std::min(1.0,control);
+            updateFireView();
+        }
     }
 
-    void updateFireView(){
+    void updateFireView() {
 
+        double not_used=0;
         /*更新数据*/
         auto data_p=fireData.data;
         for (int i=1; i<(rows-1); ++i) {
@@ -106,29 +74,57 @@ public:
             data_p+=fireData.step;
             /*当前行的指针*/
             auto p=reinterpret_cast<double*>(data_p)+1;
-            for (int j=1; j<(cols-1);++j,++p,++last_p) {
-                *p+=(*(last_p-1)+(*last_p)+*(last_p+1));
-                (*p)*=(1/4.008);
+            for (int j=1; j<(cols-1); ++j,++p,++last_p) {
+
+                /*control 控制偏向*/
+                *p+=(*(last_p-1))+
+                    (*last_p)+
+                    (*(last_p+1))+
+                    .5*(1-control)*(*(p+1))+
+                    .5*control*(*(p-1));
+
+                (*p)*=(1./(.5+4+.025));
+
+                /*进行数据裁剪到[0-1]*/
+                *p=std::modf(*p,&not_used);
+
             }
         }
 
-        cv::Mat mat(rows,cols,CV_8UC1,
-            fireImage.bits(),fireImage.bytesPerLine());
+        cv::Mat mat;
 
-        cv::normalize(
-            fireData,mat,
-            255.0,0.0,
-            cv::NORM_MINMAX,
-            CV_8UC1);
+        fireData.convertTo(mat,CV_8UC1,255);
 
         cv::flip(mat,mat,0);
 
+        cv::applyColorMap(mat,mat,cv::COLORMAP_JET);
+        cv::Mat rgbaMat(
+            fireImage.height(),
+            fireImage.width(),
+            CV_8UC4,
+            const_cast<uchar *>(fireImage.constBits()),
+            fireImage.bytesPerLine());
+
+        cv::cvtColor(mat,rgbaMat,cv::COLOR_BGR2RGBA);
+
         this->setImage(fireImage);
+
+        /*
+        使用ROI进行裁剪
+        加速衰减
+        */
+        fireData.rowRange(cv::Range(256,rows-1))=cv::Scalar(0,0,0,0);
+        fireData.rowRange(cv::Range(200,256))*=0.97+(rand()&1023)/102400.0;
+        fireData.rowRange(cv::Range(128,200))*=0.98+(rand()&1023)/102400.0;
+        fireData.rowRange(cv::Range(64,128))*=0.99+(rand()&1023)/102400.0;
+
     }
 
 private:
     CPLUSPLUS_OBJECT(FireImageShowWidget)
 };
+
+}
 
 void MainWindow::openLua() {
     this->addWidget(new FireImageShowWidget);
