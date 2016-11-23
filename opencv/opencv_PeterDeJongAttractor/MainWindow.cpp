6083be84b1042c1faabd59fa6dfbe900ac7725b4
ngzHappy <819869472@qq.com>
2016-11-23 11:16:54 +0800

    +picture

--- opencv/opencv_PeterDeJongAttractor/MainWindow.cpp
+++ opencv/opencv_PeterDeJongAttractor/MainWindow.cpp
@@ -1,21 +1,21 @@
 ﻿/*MainWindow.cpp*/
-#include <vector>
+#include <cmath>
 #include "MainWindow.hpp"
 #include <OpenCVUtility.hpp>
 
-class MainWindow::_PrivateMainWindow{
+class MainWindow::_PrivateMainWindow {
 public:
 private:
     CPLUSPLUS_OBJECT(_PrivateMainWindow)
 };
 
-MainWindow::MainWindow(QWidget *parent) :
-    _Super(parent){
+MainWindow::MainWindow(QWidget *parent):
+    _Super(parent) {
     _thisp=new _PrivateMainWindow;
 }
 
-MainWindow::~MainWindow(){
-     delete _thisp;
+MainWindow::~MainWindow() {
+    delete _thisp;
 }
 
 /*write your code here*/
@@ -23,112 +23,91 @@ QWidget* MainWindow::addImage(const QImage &arg) {
     return _Super::addImage(arg);
 }
 
-namespace  {
-template<typename T>
-using vector=std::vector<T,memory::Allocator<T>>;
-}
+namespace {
 
-#include <cmath>
-#include <cstdlib>
-
-void MainWindow::openLua(){
-    //return _Super::openLua();
+void add_image(MainWindow *view) try {
 
     constexpr auto rows=1024;
     constexpr auto cols=1024;
-    constexpr double step_y=4./rows;
-    constexpr double step_x=4./cols;
+    constexpr auto posx_max=cols-1;
+    constexpr auto posy_max=rows-1;
+    constexpr double step_x=cols/(2.-(-2.));
+    constexpr double step_y=rows/(2.-(-2.));
 
-    vector<vector<int>> hist;
-    hist.resize(rows);
-    for(auto &i:hist){
-        i.resize(cols);
-        
-    }
-      
-  //    auto a=-10.7;
-  // auto b=-1.09;
-  //   auto c=-.8156;
-  //auto d=-21.12;
+    /*点出现概率的统计矩阵*/
+    cv::Mat hist=cv::Mat::zeros(rows,cols,CV_64FC1);
 
-  
-   auto a=-12.7;
-   auto b=-1.09;
-   auto c=-.816;
-   auto d=-2.12;
- 
-
-    auto posx_max=cols-1;
-    auto posy_max=rows-1;
-    auto count_max=1.;
-
-    std::ofstream ofs("xxx.txt");
-
-
+    /* 设置初始化值 */
+    constexpr auto a=-12.7;
+    constexpr auto b=-1.09;
+    constexpr auto c=-.816;
+    constexpr auto d=-2.12;
     auto x=20.1111242432;
     auto y=564.447152;
 
-    for(int k=0;k<10240;++k)
-    for (int i=0; i<(1024*1024);++i) {
-        
-        auto xn=std::sin(a*y)-std::cos(b*x);
-        auto yn=std::sin(c*x)-std::cos(d*y);
+    /*迭代生成图片*/
+    for (int k=0; k<10; ++k)
+        for (int i=0; i<(1024*1024); ++i) {
 
- /*       if ((i&255)==0) {
-            a+=(std::rand()&4095)/4096.;
-            c+=(std::rand()&4095)/4096.;
-            d+=(std::rand()&4095)/4096.;
-            b+=(std::rand()&4095)/4096.;
-        }*/
+            auto xn=std::sin(a*y)-std::cos(b*x);
+            auto yn=std::sin(c*x)-std::cos(d*y);
 
-        auto posx=static_cast<int>( std::max(0.0, (xn+2)/step_x) );
-        auto posy=static_cast<int>( std::max(0.0, (yn+2)/step_y ) );
+            auto posx=static_cast<int>(std::max(0.0,(xn+2)*step_x));
+            auto posy=static_cast<int>(std::max(0.0,(yn+2)*step_y));
 
-       // ofs<<xn<<","<<yn<<std::endl;
+            posx=std::min(posx,posx_max);
+            posy=std::min(posy,posy_max);
 
-        posx=std::min(posx,posx_max);
-        posy=std::min(posy,posy_max);
+            auto &currentHist=hist.at<double>(posy,posx);
+            ++currentHist;
 
-        auto &currentHist=hist[posy][posx];
-        ++currentHist;
+            x=xn;
+            y=yn;
 
-        count_max=std::max<decltype(1.)>(count_max,currentHist);
-
-        x=xn ;
-        y=yn ;
-
-    }
-
-    count_max = 255.6/count_max;
-
-    cv::Mat mat(rows,cols,CV_8UC1);
-
-    auto datai= mat.data;
-    auto step=mat.step[0];
-    for (auto i=0; i<rows;++i,datai+=step) {
-        auto dataj=datai;
-        for (auto j=0; j<cols;++j,++dataj) {
-            auto &currentHist=hist[i][j];
-            *dataj=(unsigned char)(static_cast<int>(
-                currentHist*count_max));
-             
         }
-    }
 
-    {
-        QOpencvImage image;
-        image.fromOpencvMat(mat);
-        this->addImage(image);
-    }
+    /*分配灰度图内存*/
+    QImage ansGray(cols,rows,QImage::Format_Grayscale8);
+    /*对灰度图内存的引用*/
+    cv::Mat matGray(ansGray.height(),ansGray.width(),
+        CV_8UC1,ansGray.bits(),ansGray.bytesPerLine());
+    /*将统计图归一化到[0,255],生成灰度图*/
+    cv::normalize(
+        hist,matGray,
+        255.0,0.0,
+        cv::NORM_MINMAX,
+        CV_8UC1);
+    /*直方图归一化*/
+    cv::equalizeHist(matGray,matGray);
 
-    cv::equalizeHist(mat,mat);
+    /*将灰度图加入图片到绘制体系*/
+    view->addImage(ansGray);
 
-    {
-        QOpencvImage image;
-        image.fromOpencvMat(mat);
-        this->addImage(image);
-    }
+    /*分配彩色图内存*/
+    QImage ansRGB(cols,rows,QImage::Format_RGB888);
+    ansRGB.fill(QColor(0,0,0,255));
 
+    /*引用彩色图片内存*/
+    cv::Mat matRGB(ansRGB.height(),ansRGB.width(),
+        CV_8UC3,ansRGB.bits(),ansRGB.bytesPerLine());
+
+    /*灰度图重映射*/
+    cv::applyColorMap(matGray,matRGB,cv::COLORMAP_COOL);
+
+    /*将opencv bgr色系转为rgb色系(opencv架构级错误,默认用bgr而不是rgb)*/
+    cv::cvtColor(matRGB,matRGB,cv::COLOR_BGR2RGB);
+
+    /*将灰度图加入图片到绘制体系*/
+    view->addImage(ansRGB);
+}
+catch (...) {
+    CPLUSPLUS_EXCEPTION(false);
+}
+
+}/*namespace*/
+
+void MainWindow::openLua() {
+    add_image(this);
 }
 
 /*End of the file.*/
