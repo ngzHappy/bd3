f68581314adb3e1c18ab43189ae122333827e341
ngzHappy <819869472@qq.com>
2016-11-25 16:02:08 +0800

    +final

--- opencv/opencv_jpegn/MainWindow.cpp
+++ opencv/opencv_jpegn/MainWindow.cpp
@@ -44,9 +44,21 @@ public:
             try {
 
                 QImage image=arg.copy();
-              
+                auto imageData=const_cast<uchar*>(image.constBits());
+                auto lineStep=image.bytesPerLine();
+                auto imageHeight=image.height();
+                auto imageWidth3=image.width()*3;
+                auto lineEnd=imageData+imageHeight*lineStep;
+                std::vector<std::uint8_t> buffer;
 
-            
+                std::vector<int> par={
+                    CV_IMWRITE_JPEG_QUALITY,75,
+                };
+                cv::Mat input(imageHeight,image.width(),CV_8UC3,
+                    imageData,lineStep);
+
+                /*opencv架构级错误，导致大量无用运算*/
+                cv::cvtColor(input,input,cv::COLOR_RGB2BGR);
 
                 /*参考:*/
                 /*view-source:https://m13253.github.io/JPEGreen/*/
@@ -65,25 +77,15 @@ public:
 
                 while ((--i)>=0) {
 
-                    auto imageData=const_cast<uchar*>(image.constBits());
-                    auto lineStep=image.bytesPerLine();
-                    auto imageHeight=image.height();
-                    auto imageWidth3=image.width()*3;
-                    auto lineEnd=imageData+imageHeight*lineStep;
-                    std::vector<std::uint8_t> buffer;
-                    std::vector<int> par={ 
-                        CV_IMWRITE_JPEG_QUALITY,77,
-                    };
-
-                    /*RGB 与 YUV 第精度转换*/
+                    /*RGB 与 YUV 低精度转换*/
                     auto line=imageData;
                     for (; line<lineEnd; line+=lineStep) {
                         auto p=line; auto pend=p+imageWidth3;
                         for (; p<pend; p+=3) {
 
-                            const auto &r=p[0];
+                            const auto &b=p[0];
                             const auto &g=p[1];
-                            const auto &b=p[2];
+                            const auto &r=p[2];
 
                             const auto y=clamp((77*r+150*g+29*b)>>8);
                             const auto u=clampuv(((-43*r-85*g+128*b)>>8)-1);
@@ -96,20 +98,18 @@ public:
                         }
                     }
 
-                    QByteArray array_;
-                    {
-                        QBuffer buffer_(&array_);
-                        image.save(&buffer_,"jpeg",76);
-                    }
-                    {
-                        QBuffer buffer_(&array_);
-                        image.load(&buffer_,"jpeg");
-                        image=image.convertToFormat(QImage::Format_RGB888);
-                    }
-                    
-             
-                 //   assert(imageData==input.data);
+                    /*opencv架构级别错误，导致大量无用运算，
+                    尤其是save/load图片过程中，第三方库一般是按照rgb设计的，
+                    于是opencv要先将bgr转rgb，浪费大量时间
+                    可以自行查看opencv源代码验证*/
+                    cv::imencode(".jpeg",input,buffer,par);
+                    input=cv::imdecode(buffer,CV_LOAD_IMAGE_COLOR,&input);
+
+                    assert(imageData==input.data);
                 }
+
+                /*opencv架构级错误，导致大量无用运算*/
+                cv::cvtColor(input,input,cv::COLOR_RGB2BGR);
 
                 return std::move(image);
             }
